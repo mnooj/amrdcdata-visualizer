be6ef96d3663169fcee78139d01bea6700333a24
mnooj <81250253+mnooj@users.noreply.github.com>
2022-04-29 12:12:12 -0500

    Add files via upload

---
+++ writeCSV.py
@@ -0,0 +1,16 @@
+import csv 
+
+def writeCSV(datafile, targetFilepath):
+    data = open(datafile, 'r')              ## Open existing .txt file
+    header = ['Year', 'Julian Day', 'Month', 'Day', 'Observation Time', 'Temperature', 'Pressure',
+        'Wind Speed', 'Wind Direction', 'Relative Humidity', 'Delta-T']     ## Custom header
+
+    ## Create new .csv file; decode UTF characters (writer expects ASCII)
+    with open(targetFilepath + '.csv', 'w', encoding='UTF8', newline='') as newFile:
+        writer = csv.writer(newFile)        ## Create writer
+        writer.writerow(header)             ## Write our custom header to csv
+        for line in range(2):
+            next(data)                      ## Skip header from .txt file
+        for line in data:           
+            row = line.split()              ## Split() cuts out all whitespaces and creates a clean array
+            writer.writerow(row)            ## Write the array to a comma delineated row
