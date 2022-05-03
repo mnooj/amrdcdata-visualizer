a9135536e78d0856ffb4b34cd8bf7886af8f8e1d
mnooj <81250253+mnooj@users.noreply.github.com>
2022-05-03 11:22:16 -0500

    Update writeCSV.py

--- writeCSV.py
+++ writeCSV.py
@@ -1,16 +1,17 @@
-import csv 
+import csv
+import sys
 
-def writeCSV(datafile, targetFilepath):
-    data = open(datafile, 'r')              ## Open existing .txt file
-    header = ['Year', 'Julian Day', 'Month', 'Day', 'Observation Time', 'Temperature', 'Pressure',
-        'Wind Speed', 'Wind Direction', 'Relative Humidity', 'Delta-T']     ## Custom header
-
-    ## Create new .csv file; decode UTF characters (writer expects ASCII)
-    with open(targetFilepath + '.csv', 'w', encoding='UTF8', newline='') as newFile:
-        writer = csv.writer(newFile)        ## Create writer
-        writer.writerow(header)             ## Write our custom header to csv
-        for line in range(2):
-            next(data)                      ## Skip two line header from .txt file
-        for line in data:           
-            row = line.split()              ## Split() cuts out all whitespaces and creates an array of strings
-            writer.writerow(row)            ## Write the array to a comma delineated row
+targetFile = sys.argv[1]                   ## Get filename from command line arg
+data = open(targetFile, 'r')              ## Open existing .txt file
+header = ['Year', 'Julian Day', 'Month', 'Day', 'Observation Time', 'Temperature', 'Pressure',
+    'Wind Speed', 'Wind Direction', 'Relative Humidity', 'Delta-T']     ## Custom header
+targetFilepath, extension = targetFile.split('.')
+## Create new .csv file; decode UTF characters (writer expects ASCII)
+with open(targetFilepath + '.csv', 'w', encoding='UTF8', newline='') as newFile:
+    writer = csv.writer(newFile)        ## Create writer
+    writer.writerow(header)             ## Write our custom header to csv
+    for line in range(2):
+        next(data)                      ## Skip header from .txt file
+    for line in data:
+        row = line.split()              ## Split() cuts out all whitespaces and creates a clean array
+        writer.writerow(row)            ## Write the array to a comma delineated row
