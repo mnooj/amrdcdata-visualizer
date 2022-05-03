5dc30b1305665a779a56d567d9cd881a0d1eb758
mnooj <81250253+mnooj@users.noreply.github.com>
2022-05-03 11:28:19 -0500

    Update writeCSV.py

--- writeCSV.py
+++ writeCSV.py
@@ -1,17 +1,25 @@
+## Place writeCSV.py in a directory, e.g. '~/Users/Desktop', along with an AWS .txt file, e.g. "10min-gil201601q10.txt"
+## Open terminal and run the following command:
+## python3 writeCSV.py "10min-gil201601q10.txt"
+## The script will generate "10min-gil201601q10.csv" in the same directory
+
 import csv
 import sys
 
-targetFile = sys.argv[1]                   ## Get filename from command line arg
-data = open(targetFile, 'r')              ## Open existing .txt file
+targetFile = sys.argv[1]                    ## Get filename from command line arg
+data = open(targetFile, 'r')                ## Open existing .txt file
+
 header = ['Year', 'Julian Day', 'Month', 'Day', 'Observation Time', 'Temperature', 'Pressure',
     'Wind Speed', 'Wind Direction', 'Relative Humidity', 'Delta-T']     ## Custom header
-targetFilepath, extension = targetFile.split('.')
+
+destFilepath, extension = targetFile.split('.')                         ## Get base filename
+
 ## Create new .csv file; decode UTF characters (writer expects ASCII)
-with open(targetFilepath + '.csv', 'w', encoding='UTF8', newline='') as newFile:
-    writer = csv.writer(newFile)        ## Create writer
-    writer.writerow(header)             ## Write our custom header to csv
+with open(destFilepath + '.csv', 'w', encoding='UTF8', newline='') as newFile:
+    writer = csv.writer(newFile)            ## Create writer
+    writer.writerow(header)                 ## Write our custom header to csv
     for line in range(2):
-        next(data)                      ## Skip header from .txt file
+        next(data)                          ## Skip header from .txt file
     for line in data:
-        row = line.split()              ## Split() cuts out all whitespaces and creates a clean array
-        writer.writerow(row)            ## Write the array to a comma delineated row
+        row = line.split()                  ## Split() cuts out all whitespaces and creates a clean array
+        writer.writerow(row)                ## Write the array to a comma delineated row
